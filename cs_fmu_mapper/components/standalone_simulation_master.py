78ef9290eb4cb3a98421544e514d06f3f5550e3d
Frederik Bornemann <frederik.bornemann@buildlinx.de>
2024-07-17 00:33:58 +0200

    add progress bar to StandaloneSimulationMaster

--- cs_fmu_mapper/components/standalone_simulation_master.py
+++ cs_fmu_mapper/components/standalone_simulation_master.py
@@ -1,4 +1,5 @@
 from cs_fmu_mapper.components.master_component import MasterComponent
+from tqdm import tqdm
 
 
 class StandaloneSimulationMaster(MasterComponent):
@@ -8,8 +9,15 @@ class StandaloneSimulationMaster(MasterComponent):
     def __init__(self, config, name) -> None:
         super().__init__(config, name)
         self._tend = None
+        self._dt = config["timeStepPerCycle"]
         if "tend" in self._config.keys():
             self._tend = self._config["tend"]
+            self._pbar = tqdm(
+                total=self._tend,
+                unit="s",
+                bar_format="{l_bar}{bar}| {n_fmt}{unit}/{total_fmt}{unit} [{elapsed}<{remaining}]",
+            )
+            self._pbar_update_counter = 0
 
     async def run(self):
         await self.initialize()
@@ -18,7 +26,16 @@ class StandaloneSimulationMaster(MasterComponent):
 
             await self.do_step(None, None)
 
+            if self._tend is not None:
+                self._pbar_update_counter = self._pbar_update_counter + 1
+                if self._pbar_update_counter == int(1 / self._dt):
+                    self._pbar.update(1)
+                    self._pbar_update_counter = 0
+
             if self._tend is not None and self.get_time() >= self._tend:
                 break
 
         await self.finalize()
+
+        if self._tend is not None:
+            self._pbar.close()
