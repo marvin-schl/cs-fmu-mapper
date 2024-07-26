18abc1d6936536340bc3cf19b6ba98626d2249f4
Frederik Bornemann <frederik.bornemann@buildlinx.de>
2024-07-26 20:49:00 +0200

    update progress bar in scenario.py

--- cs_fmu_mapper/components/scenario.py
+++ cs_fmu_mapper/components/scenario.py
@@ -1,9 +1,9 @@
-import pandas as pd
-from tqdm import tqdm
 import os
-from cs_fmu_mapper.utils import chooseFile
-import logging
+
+import pandas as pd
 from cs_fmu_mapper.components.simulation_component import SimulationComponent
+from cs_fmu_mapper.utils import chooseFile
+from tqdm import tqdm
 
 
 class Scenario(SimulationComponent):
@@ -12,7 +12,7 @@ class Scenario(SimulationComponent):
 
     def __init__(self, config, name):
         super(Scenario, self).__init__(config, name)
-        self._log.info("Using Scneario path: " + config["path"])
+        self._log.info("Using Scenario path: " + config["path"])
         if os.path.exists(config["path"]):
             if os.path.isfile(config["path"]):
                 self._scenario = pd.read_csv(config["path"], delimiter=";")
@@ -24,16 +24,31 @@ class Scenario(SimulationComponent):
                 self._scenario = pd.read_csv(config["path"] + "/" + file, delimiter=";")
         else:
             raise FileNotFoundError("Scenario file not found at: " + config["path"])
-        self._finished = False
-        self._final_time = self._scenario.sort_values(by="t", ascending=False).iloc[0][
-            "t"
-        ]
+        self._is_finished = False
+        self._final_time = int(
+            self._scenario.sort_values(by="t", ascending=False).iloc[0]["t"]
+        )
+        # Progress bar
+        self._pbar = None
+        self._pbar_update_counter = 0
+
+    def create_progress_bar(self):
         self._pbar = tqdm(
             total=self._final_time,
             unit="s",
             bar_format="{l_bar}{bar}| {n_fmt}{unit}/{total_fmt}{unit} [{elapsed}<{remaining}]",
+            dynamic_ncols=True,
+            colour="green",
         )
-        self._pbar_update_counter = 0
+
+    def update_progress_bar(self, dt):
+        if dt >= 1:
+            self._pbar.update(dt)
+        else:
+            self._pbar_update_counter += 1
+            if self._pbar_update_counter == int(1 / dt):
+                self._pbar.update(1)
+                self._pbar_update_counter = 0
 
     def set_input_values(self, new_val):
         raise NotImplementedError("Scenario does not provide input values.")
@@ -44,10 +59,11 @@ class Scenario(SimulationComponent):
                 return name
 
     async def do_step(self, t, dt):
-        self._pbar_update_counter = self._pbar_update_counter + 1
-        if self._pbar_update_counter == int(1 / dt):
-            self._pbar.update(1)
-            self._pbar_update_counter = 0
+        if self._is_finished:
+            return
+        if self._pbar is None:
+            self.create_progress_bar()
+        self.update_progress_bar(dt)
         try:
             cur_val = (
                 self._scenario[self._scenario["t"] >= t]
@@ -66,8 +82,8 @@ class Scenario(SimulationComponent):
             )
 
         except Exception as e:
-            print(e)
-            self._finished = True
+            self._log.debug(e)
+            self._is_finished = True
             self._pbar.close()
             self._log.info("Scenario finished at t=" + str(t))
 
@@ -75,4 +91,4 @@ class Scenario(SimulationComponent):
         return True
 
     def is_finished(self):
-        return self._finished
+        return self._is_finished
