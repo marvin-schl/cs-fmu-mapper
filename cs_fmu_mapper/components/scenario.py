1c057df39370bf00131e3f5257da717c63fd2b36
Frederik Bornemann <frederik.bornemann@buildlinx.de>
2024-10-07 18:17:11 +0200

    add support for multiple parallel scenarios and change the scenario column name to the var name instead of nodeID

--- cs_fmu_mapper/components/scenario.py
+++ cs_fmu_mapper/components/scenario.py
@@ -12,34 +12,45 @@ class Scenario(SimulationComponent):
 
     def __init__(self, config, name):
         super(Scenario, self).__init__(config, name)
-        self._log.info("Using Scenario path: " + config["path"])
-        if os.path.exists(config["path"]):
-            if os.path.isfile(config["path"]):
-                self._scenario = pd.read_csv(config["path"])
-            elif os.path.isdir(config["path"]):
-                file = chooseFile(
-                    config["path"],
-                    "Scenario path is a directory. Please choose a Scenraio file:",
-                )
-                self._scenario = pd.read_csv(config["path"] + "/" + file)
-        else:
-            raise FileNotFoundError("Scenario file not found at: " + config["path"])
-        assert "t" in self._scenario.columns, "Scenario file must contain a column 't'."
+        self._scenarios = []
+        self._load_scenarios(config["path"])
 
         self._is_finished = False
         self._progress = 0
-        self._final_time = int(
-            self._scenario.sort_values(by="t", ascending=False).iloc[0]["t"]
+        self._final_time = self._calculate_final_time()
+        self._log.info(f"Final time of Scenario is: {self._final_time}s")
+
+    def _load_scenarios(self, paths):
+        if isinstance(paths, str):
+            paths = [paths]
+
+        for path in paths:
+            self._log.info(f"Loading scenario from: {path}")
+            if os.path.exists(path):
+                if os.path.isfile(path):
+                    scenario = pd.read_csv(path)
+                elif os.path.isdir(path):
+                    file = chooseFile(
+                        path,
+                        "Scenario path is a directory. Please choose a Scenario file:",
+                    )
+                    scenario = pd.read_csv(os.path.join(path, file))
+            else:
+                raise FileNotFoundError(f"Scenario file not found at: {path}")
+
+            assert (
+                "t" in scenario.columns
+            ), f"Scenario file {path} must contain a column 't'."
+            self._scenarios.append(scenario)
+
+    def _calculate_final_time(self):
+        return max(
+            int(scenario.sort_values(by="t", ascending=False).iloc[0]["t"])
+            for scenario in self._scenarios
         )
-        self._log.info(f"Final time of Scneario is: {self._final_time}s")
 
     def set_input_values(self, new_val):
         raise NotImplementedError("Scenario does not provide input values.")
-
-    def _get_name_by_node(self, nodeID):
-        for name in self._config["outputVar"].keys():
-            if self._config["outputVar"][name]["nodeID"] == nodeID:
-                return name
 
     async def do_step(self, t, dt):
         if self._is_finished:
@@ -47,25 +58,29 @@ class Scenario(SimulationComponent):
 
         try:
             self._progress = t / self._final_time
-            cur_val = (
-                self._scenario[self._scenario["t"] >= t]
-                .sort_values(by="t", ascending=True)
-                .iloc[0]
-            )
-            output_values = cur_val.to_dict()
-            del output_values["t"]
-            self.set_output_values(
-                dict(
-                    map(
-                        lambda x: (x, output_values[self.get_node_by_name(x)]),
-                        self.get_output_values().keys(),
-                    )
+            output_values = {}
+
+            for scenario in self._scenarios:
+                cur_val = (
+                    scenario[scenario["t"] >= t]
+                    .sort_values(by="t", ascending=True)
+                    .iloc[0]
                 )
+                scenario_values = cur_val.to_dict()
+                del scenario_values["t"]
+                output_values.update(scenario_values)
+
+            self.set_output_values(
+                {
+                    x: output_values[x]
+                    for x in self.get_output_values().keys()
+                    if x in output_values
+                }
             )
         except Exception as e:
             self._log.debug(e)
             self._is_finished = True
-            self._log.info("Scenario finished at t=" + str(t))
+            self._log.info(f"Scenario finished at t={t}")
 
     async def finalize(self):
         return True
