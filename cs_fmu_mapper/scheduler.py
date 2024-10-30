679293f5b7a36150bd33843ac4ad8ee149181191
Frederik Bornemann <frederik.bornemann@buildlinx.de>
2024-10-30 11:14:06 +0100

    improve Scheduler

--- cs_fmu_mapper/scheduler.py
+++ cs_fmu_mapper/scheduler.py
@@ -19,7 +19,7 @@ class Scheduler:
         self.default_items = default_items
         self.column_prefix = column_prefix
 
-    def generate_schedule(self, **kwargs) -> pd.DataFrame:
+    def generate_schedule(self, **kwargs):
         """
         Generate a schedule based on the specified parameters.
 
@@ -39,51 +39,63 @@ class Scheduler:
         - `generate_schedule(duration=48 * 3600, items=["item1", "item2"], patterns={"item1": [1, 0, 1], "item2": [0, 1, 0]})`
         This will generate a schedule for `item1` and `item2` with the pattern `[1, 0, 1]` and `[0, 1, 0]` respectively, streched evenly over 24 hours and repeated over 2 days (48 hours).
         """
-        times = kwargs.setdefault("times", None)
         duration = kwargs.setdefault("duration", 4 * 86400)
         items = kwargs.setdefault("items", self.default_items)
         patterns = kwargs.setdefault("patterns", self.default_patterns)
 
-        if times and not all([len(times) == len(patterns[item]) for item in items]):
-            raise ValueError("Length of `times` and `patterns` must be the same.")
+        assert all(
+            len(pattern) == len(next(iter(patterns.values())))
+            for pattern in patterns.values()
+        ), "All patterns must have the same length."
+
+        times = kwargs.setdefault(
+            "times",
+            [
+                int(i * (24 / len(next(iter(patterns.values())))))
+                for i in range(len(next(iter(patterns.values()))))
+            ],
+        )
+
+        assert len(times) == len(
+            next(iter(patterns.values()))
+        ), "Length of `times` and `patterns` must be the same."
 
         t = np.arange(0, duration + 1, 3600)  # Hourly data points
         df = pd.DataFrame({"t": t})
 
+        kwargs["patterns"] = {}
         for i, item in enumerate(items, 1):
-            pattern = self._get_pattern(item, str(i), patterns, kwargs)
-            values = self._generate_values(times, pattern, duration, len(t))
+            pattern = self._get_pattern(item, str(i), patterns)
+            kwargs["patterns"][item] = pattern
+            values = self._generate_values(pattern, duration, len(t), times)
             df[f"{self.column_prefix}{item}"] = values
 
         return df, kwargs
 
     @staticmethod
     def _get_pattern(
-        item: str, index: str, patterns: Dict[str, List[int]], kwargs: Dict[str, Any]
+        item: str, index: str, patterns: Dict[str, List[int]]
     ) -> List[int]:
-        return kwargs.get(
-            item, kwargs.get(index, patterns.get(index, patterns.get(item, [])))
-        )
+        return patterns.get(item, patterns.get(index, []))
 
     @staticmethod
     def _generate_values(
-        times: List[int], pattern: List[int], duration: int, time_points: int
+        pattern: List[int],
+        duration: int,
+        time_points: int,
+        times: List[int],
     ) -> np.ndarray:
-        if times:
-            daily_pattern = np.full(24, np.nan)
+        daily_pattern = np.full(24, np.nan)
 
-            for time, value in zip(times, pattern):
-                daily_pattern[time] = value
+        for time, value in zip(times, pattern):
+            daily_pattern[time] = value
 
-            for i in range(len(daily_pattern)):
-                if np.isnan(daily_pattern[i]):
-                    if i == 0:
-                        daily_pattern[i] = daily_pattern[times[-1]]
-                    else:
-                        daily_pattern[i] = daily_pattern[i - 1]
-        else:
-            seconds_per_segment = 86400 // len(pattern)
-            daily_pattern = np.repeat(pattern, seconds_per_segment // 3600)
+        for i in range(len(daily_pattern)):
+            if np.isnan(daily_pattern[i]):
+                if i == 0:
+                    daily_pattern[i] = daily_pattern[times[-1]]
+                else:
+                    daily_pattern[i] = daily_pattern[i - 1]
 
         return np.tile(daily_pattern, duration // 86400 + 1)[:time_points]
 
